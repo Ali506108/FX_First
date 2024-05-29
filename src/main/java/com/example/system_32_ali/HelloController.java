37623256fc531e50ea3d0604fbb5dfc251c2753c
Ali506108 <aliduisen77@gmail.com>
2024-05-29 09:00:32 +0500

    first commit

---
+++ src/main/java/com/example/system_32_ali/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.system_32_ali;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.system_32_ali {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.system_32_ali to javafx.fxml;
+    exports com.example.system_32_ali;
+}
